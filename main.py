a1efa3d0bf38204fffe2c12d51b024425b02035b
mike <michaellossmann@gmail.com>
2023-01-16 15:30:26 -0500

    Added logic to decode

--- main.py
+++ main.py
@@ -71,9 +71,17 @@ code_to_letter = {
     '-----': '0',
 }
 
-code = input("Enter what you want to encode as Morse Code: ").upper()
+encode_decode = input("Do you want to Encode or Decode Morse Code: ").lower()
+if encode_decode == "encode" or encode_decode == "e":
+    code = input("Enter what you want to encode as Morse Code: ").upper()
+    encoded_mc = ' '.join([letter_to_code[letter] for letter in code])
+    print(f"Your Encoded Morse Code is: {encoded_mc}")
 
-encoded_mc = ' '.join([letter_to_code[letter] for letter in code])
+elif encode_decode == "decode" or encode_decode == "d":
+    code = input("Enter the Morse Code to decode: ")
+    mc_list = code.split()
+    decoded_mc = ''.join([code_to_letter[code] for code in mc_list]).lower()
+    print(f"Your Encoded Morse Code is: {decoded_mc}")
 
-print(f"Your Encoded Morse Code is: {encoded_mc}")
-
+else:
+    print("You did not enter a valid response.")
