54deffcdc0383b66787ff76be57482babb72a3fb
mike <michaellossmann@gmail.com>
2023-01-17 15:28:12 -0500

    removed comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-letter_to_code = {
+char_to_code = {
     'A': ".-",
     'B': "-...",
     'C': '-.-.',
@@ -34,7 +34,7 @@ letter_to_code = {
     '9': '----.',
     '0': '-----',
 }
-code_to_letter = {
+code_to_char = {
     ".-": 'A',
     "-...": 'B',
     '-.-.': 'C',
@@ -74,13 +74,13 @@ code_to_letter = {
 encode_decode = input("Do you want to Encode or Decode Morse Code: ").lower()
 if encode_decode == "encode" or encode_decode == "e":
     code = input("Enter what you want to encode as Morse Code: ").upper()
-    encoded_mc = ' '.join([letter_to_code[letter] for letter in code])
+    encoded_mc = ' '.join([char_to_code[letter] for letter in code])
     print(f"Your Encoded Morse Code is: {encoded_mc}")
 
 elif encode_decode == "decode" or encode_decode == "d":
     code = input("Enter the Morse Code to decode: ")
     mc_list = code.split()
-    decoded_mc = ''.join([code_to_letter[code] for code in mc_list]).lower()
+    decoded_mc = ''.join([code_to_char[code] for code in mc_list]).lower()
     print(f"Your Encoded Morse Code is: {decoded_mc}")
 
 else:
